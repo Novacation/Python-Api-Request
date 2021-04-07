ed75bfda87e9c1bd009cee8be68845ca2f9f9539
Celestino <andersoncelestino.n@gmail.com>
2021-04-07 15:35:00 -0300

    Getting pokemons info from the api.

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+def getApiResponse():
+    try:
+        res = requests.get("https://pokeapi.co/api/v2/pokemon?limit=10&offset=200")
+
+        if res.status_code == 200:
+            print(res.json())
+
+        else:
+            raise Exception
+            
+    except:
+        print("Error!")
+
+getApiResponse()
